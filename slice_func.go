a7cf23296553f32768e5c733a869ce0ac3b6d000
Haifei.Wang <haifei.wang@dianping.com>
2017-03-02 14:06:01 +0800

    fix error

--- slice_func.go
+++ slice_func.go
@@ -1,7 +1,7 @@
 package foundation
 
 // SliceContainsStr return target slice contains s or not
-func SliceContainsStr(s string, target []string) {
+func SliceContainsStr(s string, target []string) bool {
 	for _, item := range target {
 		if s == item {
 			return true
